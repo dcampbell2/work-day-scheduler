c216746d5627bfa901a45f97f16e90ecda382c0c
Dame <dcampbell7095@gmail.com>
2020-10-07 22:26:55 -0400

    adding readme and decrementing one hour from the calender to remove the 6pm block

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # work-day-scheduler
+
+## About
+
+This project allows you to traverse the calender and place in content for you day as you prepare your day. You can use my application here - https://dcampbell2.github.io/work-day-scheduler/.
+
+## Usage
+
+When a time is in the future the time block will show as green to type in text. If the hour is currently in the present it will show as red and if its in the past it will show as grey.
+
+![Work Day Screenshot](Images/Work-Day-Screenshot.png)
--- script.js
+++ script.js
@@ -1,34 +1,47 @@
 $(document).ready(function () {
 
+// creating a variable with the begin time
   var beginTime = 9;
 
-  var endTime = 18;
+// creating a variable with the end time (both in military time) made it a lot easier to loop
+  var endTime = 17;
 
+ // creating the date and time at the very top of the page 
   var currentDateTime = moment().format("LLLL");
   console.log(currentDateTime);
 
+
+// creating a current hour 
   var getCurrentHour = parseInt(moment().format("HH"))
 
+
+// adding text to the current day id in HTML to show the current day and time
   $("#currentDay").text("The Date and Time is Now: " + currentDateTime);
 
+// making the div container class equal to a variable 
   containerEl = $(".container");
 
+// creating a new div that will be a row
   newDivRow = $("<div>");
 
   newDivRow.addClass("row");
   containerEl.append(newDivRow);
 
+  var currentTextBox;
+
+
+// dynamically creating all the content based on the number of integers between my begin time and end time integers
   for (var i = beginTime; i <= endTime; i++) {
     var getTime = moment(i, "HH").format("LT")
     
-    newDiv = $("<div>");
-    newTxtArea = $("<textarea>");
+    var newDiv = $("<div>");
+    var newTxtArea = $("<textarea>");
+    var newSaveBtn = $("<button>");
+    var newMailIcon = $("<i>");
     newDiv.addClass("time-block col-sm-12 hour row");
     newTxtArea.addClass("textarea col-sm-9");
     newTxtArea.attr("data-time", i);
-    newSaveBtn = $("<button>");
     newSaveBtn.addClass("saveBtn col-sm-2 btn btn-primary");
-    newMailIcon = $("<i>");
     newMailIcon.addClass("fas fa-save");
     newDiv.text(getTime);
     newDivRow.append(newDiv);
@@ -38,6 +51,8 @@ $(document).ready(function () {
     newSaveBtn.append(newMailIcon);
     console.log(getTime)
 
+
+// validating time to change class of elements
     if (i > parseInt(getCurrentHour)) {;
         $("textarea[data-time='" + i + "']").addClass("future");
       } else if(i === parseInt(getCurrentHour)){
@@ -48,8 +63,10 @@ $(document).ready(function () {
       }
   }
 
-  newSaveBtn.on("click", function () {
-    var storeText = localStorage.getItem("textarea");
-    localStorage.setItem("textarea", newTxtArea);
+  newSaveBtn.on("click", function (event) {
+      event.preventDefault();
+    var storeText = $("textarea").prev()
+    console.log(storeText)
+    localStorage.setItem("storedText", (storeText))
   });
 });
